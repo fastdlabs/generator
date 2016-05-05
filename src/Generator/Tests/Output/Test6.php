7ea2191c53826784b754245fbac1497f7b91c369
janhuang <384099566@qq.com>
2016-05-05 22:43:12 +0800

    add class content testing

---
+++ src/Generator/Tests/Output/Test6.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janhuang
+ * Date: 16/5/5
+ * Time: 下午10:42
+ * Github: https://www.github.com/janhuang
+ * Coding: https://www.coding.net/janhuang
+ * SegmentFault: http://segmentfault.com/u/janhuang
+ * Blog: http://segmentfault.com/blog/janhuang
+ * Gmail: bboyjanhuang@gmail.com
+ * WebSite: http://www.janhuang.me
+ */
+
+namespace Test;
+
+class Test6 implements TestInterface
+{
+    public function getName()
+    {
+        return 'janhuang';
+    }
+}
